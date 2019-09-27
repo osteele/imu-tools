daaa2754eb308d354a237fcecd8cadd84286f731
Oliver Steele <steele@osteele.com>
2019-09-27 16:36:39 +0800

    Vary the dummy data

--- src/bno055_fake.py
+++ src/bno055_fake.py
@@ -1,27 +1,53 @@
 class BNO055:
+    __counter = 1
+
     def operation_mode(self, *args):
         pass
 
     def temperature(self):
-        return 1.0
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return 1 + frac
 
     def accelerometer(self):
-        return (2.0, 2.1, 2.2)
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return (20 + frac, 21 + frac, 22 + frac)
 
     def euler(self):
-        return (3.0, 3.1, 3.2)
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return (30 + frac, 31 + frac, 32 + frac)
 
     def gravity(self):
-        return (4.0, 4.1, 4.2)
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return (40 + frac, 41 + frac, 42 + frac)
 
     def gyroscope(self):
-        return (5.0, 5.1, 5.2)
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return (50 + frac, 51 + frac, 52 + frac)
 
     def linear_acceleration(self):
-        return (6.0, 6.1, 6.2)
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return (60 + frac, 61 + frac, 62 + frac)
 
     def magnetometer(self):
-        return (7.0, 7.1, 7.2)
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return (70 + frac, 71 + frac, 72 + frac)
 
     def quaternion(self):
-        return (8.0, 8.1, 8.2)
+        frac = self.__counter / 1000
+        self.__counter += 1
+        self.__counter %= 1000
+        return (80 + frac, 81 + frac, 82 + frac)

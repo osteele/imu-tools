b3136d10af05e1858da7b4eb566e1331ecc4a195
Oliver Steele <steele@osteele.com>
2019-10-03 16:18:27 +0800

    p5js rotate[XYZ] -> applyMatrix

--- web/sketch.js
+++ web/sketch.js
@@ -1,7 +1,8 @@
 let bunny
-let rx = 0
-let ry = 0
-let rz = 0
+let zero
+let pitch = 0
+let roll = 0
+let yaw = 0
 
 function setup() {
     createCanvas(800, 800, WEBGL)
@@ -12,23 +13,28 @@ function draw() {
     background(200, 200, 212)
     noStroke()
     lights()
-    rotateX(rx)
-    rotateY(ry)
-    rotateZ(rz + Math.PI)
+
+    const c1 = Math.cos(roll)
+    const s1 = Math.sin(roll)
+    const c2 = Math.cos(pitch)
+    const s2 = Math.sin(pitch)
+    const c3 = Math.cos(yaw)
+    const s3 = Math.sin(yaw)
+    applyMatrix(
+        c2 * c3, s1 * s3 + c1 * c3 * s2, c3 * s1 * s2 - c1 * s3, 0,
+        -s2, c1 * c2, c2 * s1, 0,
+        c2 * s3, c1 * s2 * s3 - c3 * s1, c1 * c3 + s1 * s2 * s3, 0,
+        0, 0, 0, 1);
     model(bunny);
 }
 
-let zero = null
-
 onSensorData(throttled(function (data) {
-    let [e0, e1, e2] = data.euler
+    let euler = data.euler
     if (!zero) {
-        zero = [e0, e1, e2]
+        zero = [...euler]
     }
-    e0 -= zero[0]
-    e1 -= zero[1]
-    e2 -= zero[2]
-    rx = e2 * Math.PI / 180
-    ry = e0 * Math.PI / 180
-    rz = e1 * Math.PI / 180
+    zero.forEach(function (x, i) { euler[i] -= x })
+    pitch = euler[2] * Math.PI / 180
+    yaw = euler[0] * Math.PI / 180
+    roll = euler[1] * Math.PI / 180 + Math.PI
 }))

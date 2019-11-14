4b4788b9dde11d9cbc71c8f3ffb0d28d29945568
Oliver Steele <steele@osteele.com>
2019-11-14 11:25:38 +0800

    Index page fades out stale devices

--- web/index.js
+++ web/index.js
@@ -1,14 +1,28 @@
 const deviceData = {};
+let updateDeviceListTimer = null;
+
 const TABLE_HEADER = '<tr><th>Device ID</th><th>Last Seen</th></tr>';
 
 function updateDeviceList() {
     document.getElementById('device-list').innerHTML = TABLE_HEADER + Object.keys(deviceData).map(function (deviceId) {
-        const timestamp = new Date(deviceData[deviceId]);
-        return '<tr><td class="device-id">' + deviceId + '</td><td>' + timestamp + '</td></tr>';
+        const { timestamp } = deviceData[deviceId];
+        const age = Math.max(0, +new Date() - 250 - timestamp);
+        const lightness = Math.min(0.8, age / 1000);
+        const color = 'hsl(0,0%,' + lightness * 100 + '%)';
+        return '<tr><td class="device-id">' + deviceId + '</td>' +
+            '<td style="color: ' + color + '">' + new Date(timestamp) + '</td></tr>';
     }).join('');
+    if (!updateDeviceListTimer) {
+        updateDeviceListTimer = setTimeout(function () {
+            updateDeviceListTimer = null;
+            updateDeviceList();
+        }, 100)
+    }
 }
 
 onSensorData((data) => {
-    deviceData[data.device_id] = data.local_timestamp;
+    const { device_id } = data;
+    const record = deviceData[device_id] || {};
+    deviceData[device_id] = { timestamp: data.local_timestamp };
     requestAnimationFrame(updateDeviceList);
 });

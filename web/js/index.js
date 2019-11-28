475e9dbe1d6d4eb869bbba2e215421febfb4225b
Oliver Steele <steele@osteele.com>
2019-11-28 17:35:39 +0800

    Format w/ moment.js

--- web/js/index.js
+++ web/js/index.js
@@ -1,37 +1,37 @@
 import { onSensorData } from './imu-connection.js';
 const { useEffect, useState } = React;
 
-const devices = {};
+const deviceMap = {};
 
 onSensorData((data) => {
     const { device_id: id } = data;
     const timestamp = data.local_timestamp;
-    devices[id] = { id, timestamp };
+    deviceMap[id] = { id, timestamp };
 });
 
 function App() {
-    const [deviceIds, setDeviceIds] = useState([]);
+    const [devices, setDevices] = useState([]);
 
     useEffect(() => {
-        const id = setInterval(() => setDeviceIds(Object.keys(devices)));
+        const id = setInterval(() => setDevices(Object.values(deviceMap)));
         return () => clearInterval(id);
     }, [])
 
-    return deviceIds.length === 0 ? <div>No Devices</div>
+    return devices.length === 0 ? <div>No devices are online</div>
         : (
             <table className="table">
                 <tr><th>Device ID</th><th>Last Seen</th></tr>
-                {Object.values(devices).map(Device)}
+                {devices.map(Device)}
             </table>)
 }
 
 function Device({ id, timestamp }) {
     const age = Math.max(0, +new Date() - 250 - timestamp);
-    const lightness = Math.min(0.8, age / 1000);
-    const color = 'hsl(0,0%,' + lightness * 100 + '%)';
+    const brightness = Math.min(0.8, age / 1000);
+    const color = `hsl(0,0%,${brightness * 100}%)`;
     return <tr key={id}>
         <td className="device-id">{id}</td>
-        <td style={{ color }}>{String(new Date(timestamp))}</td>
+        <td style={{ color }}>{moment(new Date(timestamp)).format()}</td>
     </tr>
 }
 

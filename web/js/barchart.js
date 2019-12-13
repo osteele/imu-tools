6ab21f66034d7d08fcfa38b86dd3561b540d20c2
Oliver Steele <steele@osteele.com>
2019-12-13 10:04:01 +0800

    Change the bar chart range display

--- web/js/barchart.js
+++ web/js/barchart.js
@@ -27,12 +27,11 @@ export function draw() {
 
     let subgraphX = 10;
     let subgraphY = 10;
-    // const names = Object.keys(sensorData).filter(name => Array.isArray(sensorData[name]));
-    const names = Object.keys(sensorData).filter(
-        name => !IGNORED_PROPERTIES.includes(name)
-    );
-    names.forEach((name, i) => {
-        let values = sensorData[name];
+    const keys = Object.keys(sensorData)
+        .filter(name => !IGNORED_PROPERTIES.includes(name))
+        .sort();
+    keys.forEach((key, i) => {
+        let values = sensorData[key];
         if (!Array.isArray(values)) {
             values = [values];
         }
@@ -46,32 +45,35 @@ export function draw() {
         translate(subgraphX, subgraphY);
 
         // update the range
-        let [min, max] = ranges[name] || [0, 0];
-        // update the running max and min from the new values
-        min = Math.min.apply(null, values.concat([min]));
-        max = Math.max.apply(null, values.concat([max]));
-        ranges[name] = [min, max];
-
-        fill('gray');
-        textSize(9);
-        text(formatPrecision(max), 0, 25);
-        text(formatPrecision(min), 0, 35);
-
-        fill(PALETTE[i % PALETTE.length]);
-        textSize(14);
-        const label = name[0].toUpperCase() + name.slice(1);
-        text(label, 0, 0 + SUBGRAPH_HEIGHT + 40);
-
-        values.forEach((v, j) => {
-            const x = j * (BAR_WIDTH + 2);
-            const yMid = SUBGRAPH_HEIGHT / 2 + 25;
-            const height = (v * SUBGRAPH_HEIGHT) / 2 / Math.max(-min, max);
-            rect(x, yMid - 0.5, BAR_WIDTH, 1);
-            rect(x, yMid, BAR_WIDTH, height);
-        });
+        drawBars(key, values, PALETTE[i % PALETTE.length]);
 
         pop();
         subgraphX += subgraphWidth + 50;
+    });
+}
+
+function drawBars(key, values, barColor) {
+    const label = key[0].toUpperCase() + key.slice(1);
+
+    // update the running max and min from the new values
+    let [min, max] = ranges[key] || [0, 0];
+    min = Math.min.apply(null, values.concat([min]));
+    max = Math.max.apply(null, values.concat([max]));
+    ranges[key] = [min, max];
+
+    fill('gray');
+    textSize(9);
+    text(`${formatPrecision(min)}…${formatPrecision(max)}`, 0, 25);
+
+    fill(barColor);
+    textSize(14);
+    text(label, 0, 0 + SUBGRAPH_HEIGHT + 40);
+    values.forEach((v, i) => {
+        const x = i * (BAR_WIDTH + 2);
+        const yMid = SUBGRAPH_HEIGHT / 2 + 25;
+        const height = (v * SUBGRAPH_HEIGHT) / 2 / Math.max(-min, max);
+        rect(x, yMid - 0.5, BAR_WIDTH, 1);
+        rect(x, yMid, BAR_WIDTH, height);
     });
 }
 

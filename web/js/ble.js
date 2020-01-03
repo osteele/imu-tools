32d6e79f933a238eae7af1e948dec721e6f28135
Oliver Steele <steele@osteele.com>
2020-01-03 10:41:41 +0800

    Display sample rate, last sample, in HTML page

--- web/js/ble.js
+++ web/js/ble.js
@@ -1,9 +1,11 @@
-const UART_BT_SERVICE_ID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-const TX_BT_CHAR_ID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
-const RX_BT_CHAR_ID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+const BT_DEVICE_NAME_PREFIX = 'NYUSHIMA';
 
-const IMU_SERVICE_UUID = '509b8001-ebe1-4aa5-bc51-11004b78d5cb';
-const IMU_QUAT_CHAR_UUID = '509b8002-ebe1-4aa5-bc51-11004b78d5cb';
+const BT_UART_SERVICE_ID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+const BT_UART_TX_CHAR_ID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+const BT_UART_RX_CHAR_ID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+
+const BT_IMU_SERVICE_UUID = '509b8001-ebe1-4aa5-bc51-11004b78d5cb';
+const BT_IMU_QUAT_CHAR_UUID = '509b8002-ebe1-4aa5-bc51-11004b78d5cb';
 
 const ENC = new TextEncoder();
 const DEC = new TextDecoder();
@@ -12,17 +14,17 @@ let rxChar, txChar;
 
 async function connect() {
     let device = await navigator.bluetooth.requestDevice({
-        filters: [{ namePrefix: 'NYUSHIMA' }],
-        optionalServices: [UART_BT_SERVICE_ID, IMU_SERVICE_UUID],
+        filters: [{ namePrefix: BT_DEVICE_NAME_PREFIX }],
+        optionalServices: [BT_UART_SERVICE_ID, BT_IMU_SERVICE_UUID],
     });
     console.info('device =', device);
     const server = await device.gatt.connect();
     console.info('server =', server);
 
-    const uartService = await server.getPrimaryService(UART_BT_SERVICE_ID);
+    const uartService = await server.getPrimaryService(BT_UART_SERVICE_ID);
     console.info('uartService =', uartService);
-    rxChar = await uartService.getCharacteristic(RX_BT_CHAR_ID);
-    txChar = await uartService.getCharacteristic(TX_BT_CHAR_ID);
+    rxChar = await uartService.getCharacteristic(BT_UART_RX_CHAR_ID);
+    txChar = await uartService.getCharacteristic(BT_UART_TX_CHAR_ID);
     console.info('rx, tx =', rxChar, txChar);
 
     await rxChar.startNotifications();
@@ -35,9 +37,9 @@ async function connect() {
         }
     });
 
-    const imuService = await server.getPrimaryService(IMU_SERVICE_UUID);
+    const imuService = await server.getPrimaryService(BT_IMU_SERVICE_UUID);
     console.info('imuService =', imuService);
-    const quatChar = await imuService.getCharacteristic(IMU_QUAT_CHAR_UUID);
+    const quatChar = await imuService.getCharacteristic(BT_IMU_QUAT_CHAR_UUID);
     await quatChar.startNotifications();
     let sampleTimes = [];
     quatChar.addEventListener('characteristicvaluechanged', ({ target }) => {
@@ -52,8 +54,13 @@ async function connect() {
         const now = +new Date();
         sampleTimes = [now, ...sampleTimes.filter(n => n > now - 1000)];
         if (Math.floor(now / 1000) > Math.floor(sampleTimes[1] / 1000)) {
-            console.info(`samples: ${sampleTimes.length}/sec`);
+            // console.info(`samples: ${sampleTimes.length}/sec`);
+            document.getElementById(
+                'sample-rate'
+            ).innerText = `${sampleTimes.length} samples/sec`;
         }
+        const q = [get32(0), get32(4), get32(8), get32(12)];
+        document.getElementById('imu-q').innerText = JSON.stringify(q);
         // console.log('IMU.quat', value.getFloat32(0))
         // console.log('IMU.quat', get32(0), get32(4), get32(8), get32(12))
     });

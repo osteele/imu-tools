55aca2eae2bb5d8f6c870d88266c8360895eb06b
Oliver Steele <steele@osteele.com>
2020-01-04 22:51:19 +0800

    Read BLE device info; factor sensor payload decode

--- web/js/ble.js
+++ web/js/ble.js
@@ -5,7 +5,8 @@ const BLE_UART_TX_CHAR_ID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
 const BLE_UART_RX_CHAR_ID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 
 const BLE_IMU_SERVICE_UUID = '509b8001-ebe1-4aa5-bc51-11004b78d5cb';
-const BLE_IMU_QUAT_CHAR_UUID = '509b8002-ebe1-4aa5-bc51-11004b78d5cb';
+const BLE_IMU_SENSOR_CHAR_UUID = '509b8002-ebe1-4aa5-bc51-11004b78d5cb';
+const BLE_IMU_DEVICE_INFO_CHAR_UUID = '509b8003-ebe1-4aa5-bc51-11004b78d5cb';
 
 const BLE_IMU_QUATERNION_FLAG = 0x20;
 
@@ -20,7 +21,7 @@ const callbacks = [];
 async function connect() {
     let device = await navigator.bluetooth.requestDevice({
         filters: [{ services: [BLE_IMU_SERVICE_UUID] }],
-        optionalServices: [BLE_UART_SERVICE_ID],
+        optionalServices: [BLE_IMU_DEVICE_INFO_CHAR_UUID, BLE_UART_SERVICE_ID],
     });
     // console.info('device =', device);
     server = await device.gatt.connect();
@@ -45,41 +46,63 @@ async function connect() {
 
     const imuService = await server.getPrimaryService(BLE_IMU_SERVICE_UUID);
     // console.info('imuService =', imuService);
-    const quatChar = await imuService.getCharacteristic(BLE_IMU_QUAT_CHAR_UUID);
-    await quatChar.startNotifications();
-    quatChar.addEventListener('characteristicvaluechanged', ({ target }) => {
-        const { value } = target;
-        let i = 0;
-        const messageVersion = value.getUint8(i++);
-        if (messageVersion !== 1) return;
-        let data = {
-            deviceId: device.id,
-            receivedAt: +new Date(),
-        };
-        const flags = value.getUint8(i++);
-        i += 2;
-        if (flags & BLE_IMU_QUATERNION_FLAG) {
-            const quat = [
-                decodeFloat32(value, i),
-                decodeFloat32(value, i + 4),
-                decodeFloat32(value, i + 8),
-                decodeFloat32(value, i + 12),
-            ];
-            i += 4 * 4;
-            const [q0, q1, q2, q3] = quat;
-            const orientationMatrix = quatToMatrix(q3, q1, q0, q2);
-            data = { orientationMatrix, quaternion: quat, ...data };
+
+    const imuDeviceNameChar = await imuService.getCharacteristic(
+        BLE_IMU_DEVICE_INFO_CHAR_UUID
+    );
+    let deviceNameView = await imuDeviceNameChar.readValue();
+    const deviceName = DEC.decode(deviceNameView);
+
+    const imuSensorChar = await imuService.getCharacteristic(
+        BLE_IMU_SENSOR_CHAR_UUID
+    );
+    await imuSensorChar.startNotifications();
+    imuSensorChar.addEventListener(
+        'characteristicvaluechanged',
+        ({ target }) => {
+            let data = decodeSensorData(target.value);
+            if (!data) return;
+            data = {
+                deviceId: deviceName,
+                receivedAt: +new Date(),
+                ...data,
+            };
+            callbacks.forEach(fn => fn(data));
         }
-        callbacks.forEach(fn => fn(data));
-    });
+    );
 }
 
-function decodeFloat32(value, n) {
-    const ar = new Uint8Array(4);
-    for (let i = 0; i < 4; ++i) {
-        ar[i] = value.getUint8(n + 3 - i);
+function decodeSensorData(dataView) {
+    let data = {};
+
+    let i = 0;
+    const nextUint8 = () => dataView.getUint8(i++);
+    const nextUint16 = () => dataView.getUint16((i += 2) - 2);
+    const nextFloat32 = () => decodeFloat32(dataView, (i += 4) - 4);
+    const nextFloat32Array = n =>
+        Array(n)
+            .fill()
+            .map(nextFloat32);
+
+    const messageVersion = nextUint8();
+    if (messageVersion !== 1) return null;
+    const flags = nextUint8();
+    nextUint16();
+    if (flags & BLE_IMU_QUATERNION_FLAG) {
+        const quat = nextFloat32Array(4);
+        const [q0, q1, q2, q3] = quat;
+        const orientationMatrix = quatToMatrix(q3, q1, q0, q2);
+        data = { orientationMatrix, quaternion: quat, ...data };
     }
-    return new DataView(ar.buffer).getFloat32(0);
+    return data;
+
+    function decodeFloat32(value, n) {
+        const ar = new Uint8Array(4);
+        for (let i = 0; i < 4; ++i) {
+            ar[i] = value.getUint8(n + 3 - i);
+        }
+        return new DataView(ar.buffer).getFloat32(0);
+    }
 }
 
 function onDisconnected() {

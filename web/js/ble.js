b41002844595da80007e98e6175da0a0b6d15460
Oliver Steele <steele@osteele.com>
2020-01-04 13:28:51 +0800

    New BLE payload format

--- web/js/ble.js
+++ web/js/ble.js
@@ -1,11 +1,13 @@
 import { quatToMatrix } from './utils.js';
 
-const BT_UART_SERVICE_ID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-const BT_UART_TX_CHAR_ID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
-const BT_UART_RX_CHAR_ID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+const BLE_UART_SERVICE_ID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+const BLE_UART_TX_CHAR_ID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+const BLE_UART_RX_CHAR_ID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 
-const BT_IMU_SERVICE_UUID = '509b8001-ebe1-4aa5-bc51-11004b78d5cb';
-const BT_IMU_QUAT_CHAR_UUID = '509b8002-ebe1-4aa5-bc51-11004b78d5cb';
+const BLE_IMU_SERVICE_UUID = '509b8001-ebe1-4aa5-bc51-11004b78d5cb';
+const BLE_IMU_QUAT_CHAR_UUID = '509b8002-ebe1-4aa5-bc51-11004b78d5cb';
+
+const BLE_IMU_QUATERNION_FLAG = 0x20;
 
 const ENC = new TextEncoder();
 const DEC = new TextDecoder();
@@ -17,49 +19,56 @@ const callbacks = [];
 
 async function connect() {
     let device = await navigator.bluetooth.requestDevice({
-        filters: [{ services: [BT_IMU_SERVICE_UUID] }],
-        optionalServices: [BT_UART_SERVICE_ID],
+        filters: [{ services: [BLE_IMU_SERVICE_UUID] }],
+        optionalServices: [BLE_UART_SERVICE_ID],
     });
-    console.info('device =', device);
+    // console.info('device =', device);
     server = await device.gatt.connect();
-    console.info('server =', server);
+    // console.info('server =', server);
     document.body.className += ' connected';
 
-    const uartService = await server.getPrimaryService(BT_UART_SERVICE_ID);
+    const uartService = await server.getPrimaryService(BLE_UART_SERVICE_ID);
     // console.info('uartService =', uartService);
-    rxChar = await uartService.getCharacteristic(BT_UART_RX_CHAR_ID);
-    txChar = await uartService.getCharacteristic(BT_UART_TX_CHAR_ID);
+    rxChar = await uartService.getCharacteristic(BLE_UART_RX_CHAR_ID);
+    txChar = await uartService.getCharacteristic(BLE_UART_TX_CHAR_ID);
     // console.info('rx, tx =', rxChar, txChar);
 
     await rxChar.startNotifications();
     rxChar.addEventListener('characteristicvaluechanged', ({ target }) => {
         const msg = DEC.decode(target.value);
-        console.log('UART.Rx', msg);
+        console.log('UART.Rx:', msg);
         if (msg == 'ping') {
             transmit('pong');
         }
     });
 
-    const imuService = await server.getPrimaryService(BT_IMU_SERVICE_UUID);
+    const imuService = await server.getPrimaryService(BLE_IMU_SERVICE_UUID);
     // console.info('imuService =', imuService);
-    const quatChar = await imuService.getCharacteristic(BT_IMU_QUAT_CHAR_UUID);
+    const quatChar = await imuService.getCharacteristic(BLE_IMU_QUAT_CHAR_UUID);
     await quatChar.startNotifications();
     quatChar.addEventListener('characteristicvaluechanged', ({ target }) => {
         const { value } = target;
-        const quat = [
-            decodeFloat32(value, 6),
-            decodeFloat32(value, 10),
-            decodeFloat32(value, 14),
-            decodeFloat32(value, 18),
-        ];
-        const [q0, q1, q2, q3] = quat;
-        const orientationMatrix = quatToMatrix(q3, q1, q0, q2);
-        const data = {
+        let i = 0;
+        const messageVersion = value.getUint8(i++);
+        if (messageVersion !== 1) return;
+        let data = {
             device_id: device.id,
             local_timestamp: +new Date(),
-            orientationMatrix,
-            quaternion: quat,
         };
+        const flags = value.getUint8(i++);
+        i += 2;
+        if (flags & BLE_IMU_QUATERNION_FLAG) {
+            const quat = [
+                decodeFloat32(value, i),
+                decodeFloat32(value, i + 4),
+                decodeFloat32(value, i + 8),
+                decodeFloat32(value, i + 12),
+            ];
+            i += 4 * 4;
+            const [q0, q1, q2, q3] = quat;
+            const orientationMatrix = quatToMatrix(q3, q1, q0, q2);
+            data = { orientationMatrix, quaternion: quat, ...data };
+        }
         callbacks.forEach(fn => fn(data));
     });
 }

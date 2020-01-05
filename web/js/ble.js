14434561b657819545bc35b7a66cab2eea8aa07a
Oliver Steele <steele@osteele.com>
2020-01-05 10:51:53 +0800

    Read calibration data; renumber the BLE UUIDs

--- web/js/ble.js
+++ web/js/ble.js
@@ -4,12 +4,12 @@ const BLE_UART_SERVICE_ID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const BLE_UART_TX_CHAR_ID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
 const BLE_UART_RX_CHAR_ID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 
-const BLE_MAC_ADDRESS_SERVICE_UUID = '709f7001-37e3-439e-a338-23f00067988b';
-const BLE_MAC_ADDRESS_CHAR_UUID = '709f7002-37e3-439e-a338-23f00067988b';
+const BLE_MAC_ADDRESS_SERVICE_UUID = '709f0001-37e3-439e-a338-23f00067988b';
+const BLE_MAC_ADDRESS_CHAR_UUID = '709f0002-37e3-439e-a338-23f00067988b';
 
-const BLE_IMU_SERVICE_UUID = '509b8001-ebe1-4aa5-bc51-11004b78d5cb';
-const BLE_IMU_SENSOR_CHAR_UUID = '509b8002-ebe1-4aa5-bc51-11004b78d5cb';
-const BLE_IMU_DEVICE_INFO_CHAR_UUID = '509b8003-ebe1-4aa5-bc51-11004b78d5cb';
+const BLE_IMU_SERVICE_UUID = '509b0001-ebe1-4aa5-bc51-11004b78d5cb';
+const BLE_IMU_SENSOR_CHAR_UUID = '509b0002-ebe1-4aa5-bc51-11004b78d5cb';
+const BLE_IMU_CALIBRATION_CHAR_UUID = '509b0003-ebe1-4aa5-bc51-11004b78d5cb';
 
 const BLE_IMU_QUATERNION_FLAG = 0x20;
 
@@ -24,11 +24,7 @@ const callbacks = [];
 async function connect() {
     let device = await navigator.bluetooth.requestDevice({
         filters: [{ services: [BLE_IMU_SERVICE_UUID] }],
-        optionalServices: [
-            BLE_IMU_DEVICE_INFO_CHAR_UUID,
-            BLE_MAC_ADDRESS_SERVICE_UUID,
-            BLE_UART_SERVICE_ID,
-        ],
+        optionalServices: [BLE_MAC_ADDRESS_SERVICE_UUID, BLE_UART_SERVICE_ID],
     });
     // console.info('device =', device);
     server = await device.gatt.connect();
@@ -63,6 +59,20 @@ async function connect() {
     const imuService = await server.getPrimaryService(BLE_IMU_SERVICE_UUID);
     // console.info('imuService =', imuService);
 
+    const imuCalibrationChar = await imuService.getCharacteristic(
+        BLE_IMU_CALIBRATION_CHAR_UUID
+    );
+    let calibrationView = await imuCalibrationChar.readValue();
+    let calibration = calibrationView.getUint8(0);
+    await imuCalibrationChar.startNotifications();
+    imuCalibrationChar.addEventListener(
+        'characteristicvaluechanged',
+        ({ target }) => {
+            calibration = target.value.getUint8(0);
+            console.info('calibration', calibration);
+        }
+    );
+
     const imuSensorChar = await imuService.getCharacteristic(
         BLE_IMU_SENSOR_CHAR_UUID
     );
@@ -75,6 +85,7 @@ async function connect() {
             data = {
                 deviceId: deviceName,
                 receivedAt: +new Date(),
+                calibration,
                 ...data,
             };
             callbacks.forEach(fn => fn(data));
